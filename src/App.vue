538fd2814a751dec5aae40b950478c598c0a3ec7
BeiSen <a1@1deiMac.local>
2018-12-26 18:35:21 +0800

    work

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,32 @@
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+const getters = {
+  a: () => 1,
+  b: () => 2
+}
+function fn (keys) {
+  const data = {}
+  keys.forEach(key => {
+    if (getters.hasOwnProperty(key)) {
+      data[key] = getters[key]
+    }
+  })
+  return data
+}
+export default {
+  computed: {
+    ...mapGetters(['test']),
+    ...fn(['a', 'b'])
+  },
+  mounted () {
+    // this.$store.dispatch('setTest', 8).then(() => {
+    //   console.log(this.test)
+    // })
+    console.log(this.a, this.b)
+  }
+}
 document.addEventListener('DOMContentLoaded', () => {
   const html = document.querySelector('html')
   let fontSize = window.innerWidth / 10
