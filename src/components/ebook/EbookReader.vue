500963caa79d6f89c9e11c78a16a924e687e5244
BeiSen <a1@1deiMac.local>
2019-04-10 18:28:26 +0800

    标题栏\菜单栏

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -5,17 +5,34 @@
 </template>
 
 <script type='text/ecmascript-6'>
-import { mapGetters } from 'vuex'
+import { ebookMixin } from '../../utils/mixin'
 import Epub from 'epubjs'
 global.ePub = Epub
 export default {
-  computed: {
-    ...mapGetters(['fileName'])
-  },
+  mixins: [ebookMixin],
   methods: {
+    prevPage () {
+      if (this.rendition) {
+        this.rendition.prev()
+        this.hideTitleAndMenu()
+      }
+    },
+    nextPate () {
+      if (this.rendition) {
+        this.rendition.next()
+        this.hideTitleAndMenu()
+      }
+    },
+    showTitleAndMenu () {
+      this.setMenuVisible(!this.menuVisible)
+    },
+    hideTitleAndMenu () {
+      this.$store.dispatch('setMenuVisible', !this.menuVisible)
+      this.setMenuVisible(false)
+    },
     initEpub () {
-      const url = 'http://192.168.0.20:8081/epub/' + this.fileName + '.epub'
-      this.book = new Epub(url)
+      const baseUrl = 'http://192.168.0.41:8081/epub/' + this.fileName + '.epub'
+      this.book = new Epub(baseUrl)
       this.rendition = this.book.renderTo('read', {
         width: innerWidth,
         height: innerHeight,
@@ -23,19 +40,29 @@ export default {
       })
       this.rendition.display()
       this.rendition.on('touchstart', event => {
-        console.log(event)
+        // console.log(event)
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTime = event.timeStamp
       })
       this.rendition.on('touchend', event => {
-        console.log(event)
+        const offsetX = event.changedTouches[0].clientX - this.touchStartX
+        const tiem = event.timeStamp - this.touchStartTime
+        if (tiem < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (tiem < 500 && offsetX < -40) {
+          this.nextPate()
+        } else {
+          this.showTitleAndMenu()
+        }
+        event.preventDefault()
+        event.stopPropagation()
       })
     }
   },
   mounted () {
-    this.$store.dispatch('setFileName', this.$route.params.fileName.split('|').join('/'))
-      .then(() => {
-        this.initEpub()
-      })
-    // console.log(`${baseUrl}${fileName}.epub`)
+    this.setFileName(this.$route.params.fileName.split('|').join('/')).then(() => {
+      this.initEpub()
+    })
   }
 }
 </script>
